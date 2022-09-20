3283496b52690c9cd23117f80f3e56032b969b99
Jarek Armatys <jarekarmatys@gmail.com>
2022-09-20 08:59:52 +0200

    Lekcja nr 14. - Obsługa błędów w try catch

--- WPImporter/BotWP/Bot.cs
+++ WPImporter/BotWP/Bot.cs
@@ -25,33 +25,41 @@ namespace WPImporter.BotWP
             // TODO: w try catcha to zapakować
             var options = new ChromeOptions();
 
-            options.AddArgument("--headless");  
-            options.AddArgument("log-level=3");            
+            options.AddArgument("--headless");
+            options.AddArgument("log-level=3");
 
             using var driver = new ChromeDriver(options);
 
             driver.Navigate().GoToUrl(_URL);
 
             Thread.Sleep(3000);
-
-            var authorElement = driver.FindElement(By.Id("author"));
-            authorElement.SendKeys(author);
-
-            var commentElement = driver.FindElement(By.Id("comment"));
-            commentElement.SendKeys($"{comment} - opinia pobrana z Google wizytówki firmy {companyName}");
-
-            foreach (var category in _ratingCategories)
+            try
             {
-                var ratingElement = driver.FindElement(By.CssSelector($"label[for='{category}-{rating}']"));
-                ratingElement.Click();
+                var authorElement = driver.FindElement(By.Id("author"));
+                authorElement.SendKeys(author);
+
+                var commentElement = driver.FindElement(By.Id("comment"));
+                commentElement.SendKeys($"{comment} - opinia pobrana z Google wizytówki firmy {companyName}");
+
+                foreach (var category in _ratingCategories)
+                {
+                    var ratingElement = driver.FindElement(By.CssSelector($"label[for='{category}-{rating}']"));
+                    ratingElement.Click();
+                }
+
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.Id("submit")).Click();
+
+                Console.WriteLine($"Dodałem komentarz autora: {author} dla firmy: {companyName}");
+
+                // Aby WordPress nie wykrył "ataku" czyli zbyt dużej ilości requestów na końcu czekamy 10 sekund
+                Thread.Sleep(10000);
             }
-            
-            Thread.Sleep(2000);
-
-            driver.FindElement(By.Id("submit")).Click();
-
-            // Aby WordPress nie wykrył "ataku" czyli zbyt dużej ilości requestów na końcu czekamy 10 sekund
-            Thread.Sleep(10000);
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd w dowaniu komentarza");
+            }
         }
     }
 }
